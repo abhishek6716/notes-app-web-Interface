ffe5de99f31af94e0489fd77e245d2df9715d055
Abhishek singh <abhisheksingh6716@gmail.com>
2021-06-13 08:25:29 +0530

    styling app

--- scripts/notes-fxn.js
+++ scripts/notes-fxn.js
@@ -25,18 +25,9 @@ const removeNote = (id) => {
 
 // generate DOM element
 const generateNoteDOM = (note) => {
-    const noteEl = document.createElement('div')
-    const textEl = document.createElement('a')
-    const button = document.createElement('button')
-
-    // setup the remove note button
-    button.textContent = 'x'
-    noteEl.appendChild(button)
-    button.addEventListener('click', () => {
-        removeNote(note.id)
-        saveNotes(notes)
-        renderNotes(notes, filters)
-    })
+    const noteEl = document.createElement('a')
+    const textEl = document.createElement('p')
+    const statusEl = document.createElement('p')
 
     // setup the note title text
     if (note.title.length > 0) {
@@ -44,8 +35,17 @@ const generateNoteDOM = (note) => {
     } else {
         textEl.textContent = 'Unnamed note'
     }
-    textEl.setAttribute('href', `edit.html#${note.id}`)
+    textEl.classList.add('list-item__title')
     noteEl.appendChild(textEl)
+
+    // setup link
+    noteEl.setAttribute('href', `edit.html#${note.id}`)
+    noteEl.classList.add('list-item')
+
+    // setup status message
+    statusEl.textContent = generateLastEdited(note.updatedAt)
+    statusEl.classList.add('list-item__subtitle')
+    noteEl.appendChild(statusEl) 
 
     return noteEl
 }
@@ -89,15 +89,24 @@ const sortNotes = (notes, sortBy) => {
 
 // render notes
 const renderNotes = (notes, filters) => {
+    const notesEl = document.querySelector('#notes')
     notes = sortNotes(notes, filters.sortBy)
     const filterNotes = notes.filter((note) => note.title.toLowerCase().includes(filters.searchText.toLowerCase()))
 
-    document.querySelector('#notes').innerHTML = ''
+    notesEl.innerHTML = ''
 
-    filterNotes.forEach((note) => {
-        const noteEl = generateNoteDOM(note)
-        document.querySelector('#notes').appendChild(noteEl)
-    })
+    if(filterNotes.length > 0){
+        filterNotes.forEach((note) => {
+            const noteEl = generateNoteDOM(note)
+            document.querySelector('#notes').appendChild(noteEl)
+        })
+    } else{
+        const emptyMessage = document.createElement('p')
+        emptyMessage.textContent = 'No notes to show !';
+        emptyMessage.classList.add('empty-message')
+        notesEl.appendChild(emptyMessage)
+    }
+    
 }
 
 // generate the last edited message
